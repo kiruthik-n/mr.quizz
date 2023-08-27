30a293aa86773b3cfe531a5afd2817a03a3c8acb
kiruthik-n <140431924+kiruthik-n@users.noreply.github.com>
2023-08-27 13:19:06 +0530

    Add files via upload

---
+++ qns.js
@@ -0,0 +1,67 @@
+let questions=new Array("Who Invented JavaScript ?","Which one of the following also known as Conditional Expression ?","The function and  var are known as ?","How can a datatype be declared to be a constant type ?","When an operator’s value is NULL, the typeof returned by the unary operator is ?");
+let options=new Array("Douglas Crockford","Sheryl Sandberg","Brendan Eich","Kiruthik","Alternative to if-else","Switch statement","If-then-else statement","immediate if","Keywords","Data types","Declaration statements","Prototypes","const","var","const","let","boolean","undefined","integer","object");
+let answers=new Array('c','d','c','c','d');
+let selected=new Array();
+document.getElementById("quest").innerHTML=questions[0];
+document.getElementById("aa").innerHTML=options[0];
+document.getElementById("bb").innerHTML=options[1];
+document.getElementById("cc").innerHTML=options[2];
+document.getElementById("dd").innerHTML=options[3];
+
+
+let i=1;let j=4;let score=0;
+function display(){
+
+
+
+
+ if(i<=5 & j<20){
+    document.getElementById("quest").innerHTML=questions[i];
+i++;
+document.getElementById("aa").innerHTML=options[j];
+j++;
+document.getElementById("bb").innerHTML=options[j];
+j++;
+document.getElementById("cc").innerHTML=options[j];
+j++;
+document.getElementById("dd").innerHTML=options[j];
+j++;
+ } else{
+    alert("Press Submit");
+ }
+ document.getElementsByName('demo').forEach(radio=>{
+    if(radio.checked){
+        selected.push(radio.value);
+        console.log(selected);
+        
+    }
+ });
+ 
+
+};
+
+function submit(){
+        let i=0;
+        for(let data1=0;data1<5;data1++){
+            if(selected[data1]===answers[i]){
+                score=score+1;
+            }
+            i++;
+        }
+    
+    if(score==5){
+        document.getElementById("result").innerHTML=`EXCELLENT ! 
+        Your Score is : ${score} / 5`;
+    }else if(score>=3 & score<=4){
+        document.getElementById("result").innerHTML=`GOOD !  
+        Your Score is : ${score} / 5`;
+    }else{
+        document.getElementById("result").innerHTML=`KEEP LEARNING ! 
+        Your Score is : ${score} / 5`;
+    }
+   
+        //    console.log(answers);
+           // console.log(selected);
+
+
+}
